1ab5557c2927b6e4655ebc4673ae97088a666f22
Graham Miao <grahammiao@google.com>
2024-02-12 13:35:19 -0500

    "init"

---
+++ src/components/Item.vue
@@ -0,0 +1,14 @@
+<template>
+  <el-button>Default</el-button>
+  <el-button type="primary">Primary</el-button>
+  <el-button type="success">Success</el-button>
+  <el-button type="info">Info</el-button>
+  <el-button type="warning">Warning</el-button>
+  <el-button type="danger">Danger</el-button>
+</template>
+<script>
+import { ElButton } from 'element-plus'
+export default {
+  components: { ElButton }
+}
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,5 @@
+<template>
+  <main>
+    <RiichiCounter />
+  </main>
+</template>
