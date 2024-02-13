571993721bcec17872356d01b9c4affb14903c62
Graham Miao <grahammiao@google.com>
2024-02-13 01:44:14 -0500

    save

--- src/components/RiichiCounter.vue
+++ src/components/RiichiCounter.vue
@@ -3,7 +3,9 @@
   <div v-if="!game.on_going && !game.finished">
     <div v-for="wind in Winds">
       <el-row :gutter="20">
-        <span class="ml-3 w-35 text-gray-600">{{WindsDisplayTextMap.wind_character[wind]}}起 </span>
+        <span class="ml-3 w-35 text-gray-600"
+          >{{ WindsDisplayTextMap.wind_character[wind] }}起
+        </span>
         <el-input
           v-model="game.players[wind].name"
           :placeholder="wind"
@@ -20,80 +22,93 @@
     <el-button type="danger" @click="FinishGame">结束游戏</el-button>
 
     <div>
-      <span>
-        当前局: {{ current_hand }}, {{game.honba}} 本场
-      </span>
-      <span>
-        供托: {{ game.riichi_sticks }}
-      </span>
+      <span> 当前局: {{ current_hand }}, {{ game.honba }} 本场 </span>
+      <span> 供托: {{ game.riichi_sticks }} </span>
     </div>
 
-    <div>分数:</div>
     <div v-for="wind in Winds">
+      [{{ WindsDisplayTextMap.wind_character[game.players[wind].current_wind] }}]
       {{ game.players[wind].name }}: {{ game.players[wind].points }}
     </div>
 
-
-    <div v-for="wind in Winds">
-      <el-checkbox-group v-model="game.hand_results.riichi">
-        <el-checkbox-button :label="wind" @change="HandlePlayerRiichi(wind)">
-          {{ game.players[wind].name }} 立直!
-        </el-checkbox-button>
-      </el-checkbox-group>
-    </div>
+    {{ game.hand_results.result }} game.hand_results.result
 
     <el-form :model="game.hand_results">
-      <el-form-item label="对局结果">
-        <el-select v-model="game.hand_results.result" placeholder="result" clearable>
-          <el-option label="自摸" value="tsumo" />
-          <el-option label="荣胡" value="ron" />
-          <el-option label="流局" value="draw" />
-        </el-select>
+      <el-form-item label="立直">
+        <el-checkbox-group
+          v-for="player_id in Winds"
+          v-model="game.hand_results.riichi"
+          size="default"
+        >
+          <el-checkbox-button :label="player_id" @change="HandlePlayerRiichi(player_id)">
+            {{ game.players[player_id].name }} 立直！
+          </el-checkbox-button>
+        </el-checkbox-group>
       </el-form-item>
-      <el-form-item label="听牌" v-if="game.hand_results.result == 'draw'">
+
+      <el-form-item label="对局结果">
+        <el-radio-group
+          v-for="result in HandResults"
+          v-model="game.hand_results.result"
+          size="default"
+        >
+          <el-radio-button :label="result"> {{ ResultDisplayTextMap[result] }} </el-radio-button>
+        </el-radio-group>
+      </el-form-item>
+      <el-form-item label="听牌" v-if="game.hand_results.result == HandResults.DRAW" size="default">
         <el-checkbox-group v-model="game.hand_results.tenpai">
-          <el-checkbox-button v-for="wind in Winds" :label="wind">
-            {{ game.players[wind].name }}
+          <el-checkbox-button v-for="player_id in Winds" :label="player_id">
+            {{ game.players[player_id].name }}
           </el-checkbox-button>
         </el-checkbox-group>
       </el-form-item>
       <el-form-item
         label="胡牌"
-        v-if="game.hand_results.result == 'tsumo' || game.hand_results.result == 'ron'"
+        v-if="
+          game.hand_results.result == HandResults.TSUMO ||
+          game.hand_results.result == HandResults.RON
+        "
       >
-        <el-select v-model="game.hand_results.winner" clearable>
-          <el-option v-for="wind in Winds" :label="game.players[wind].name" :value="wind" />
-        </el-select>
-      </el-form-item>
-      <el-form-item label="点炮" v-if="game.hand_results.result == 'ron'">
-        <el-select
-          v-model="game.hand_results.deal_in"
-          clearable
-          :disabled="!game.hand_results.winner"
+        <el-radio-group
+          v-for="player_id in Winds"
+          v-model="game.hand_results.winner"
+          size="default"
         >
-          <el-option
-            v-for="wind in Winds"
-            :label="game.players[wind].name"
-            :value="wind"
-            :disabled="game.players[wind].name == game.hand_results.winner"
-          />
-        </el-select>
+          <el-radio-button :label="player_id"> {{ game.players[player_id].name }} </el-radio-button>
+        </el-radio-group>
+      </el-form-item>
+      <el-form-item label="点炮" v-if="game.hand_results.result == HandResults.RON">
+        <el-radio-group
+          v-for="player_id in Winds"
+          v-model="game.hand_results.deal_in"
+          size="default"
+          :disabled="player_id == game.hand_results.winner"
+        >
+          <el-radio-button :label="player_id"> {{ game.players[player_id].name }} </el-radio-button>
+        </el-radio-group>
       </el-form-item>
       <el-form-item
         label="番"
         v-if="game.hand_results.result == 'tsumo' || game.hand_results.result == 'ron'"
       >
-        <el-select v-model="game.hand_results.han" clearable>
-          <el-option v-for="han in AllowedHans" :label="han" :value="han" />
-        </el-select>
+        <el-radio-group v-for="han in AllowedHans" v-model="game.hand_results.han" size="default">
+          <el-radio-button :label="han">{{
+            PointsLadderDisplayMap.hasOwnProperty(han) ? PointsLadderDisplayMap[han] : han
+          }}</el-radio-button>
+        </el-radio-group>
       </el-form-item>
       <el-form-item
         label="符"
         v-if="game.hand_results.result == 'tsumo' || game.hand_results.result == 'ron'"
       >
-        <el-select v-model="game.hand_results.fu" clearable>
-          <el-option v-for="fu in AllowedFus" :label="fu" :value="fu" />
-        </el-select>
+        <el-radio-group
+          v-for="fu in AllowedFus[game.hand_results.han]"
+          v-model="game.hand_results.fu"
+          size="default"
+          :disabled="PointsLadderDisplayMap.hasOwnProperty(game.hand_results.han)"
+        >
+          <el-radio-button :label="fu" />
+        </el-radio-group>
       </el-form-item>
 
       <el-form-item>
@@ -102,16 +117,26 @@
     </el-form>
   </div>
 
-  <el-table :data="game.log" style="width: 100%">
-    <el-table-column prop="hand_signature" label="场"/>
-    <el-table-column prop="east_points_delta_with_riichi" :label="game.players[Winds.EAST].name"/>
-    <el-table-column prop="south_points_delta_with_riichi" :label="game.players[Winds.SOUTH].name"/>
-    <el-table-column prop="west_points_delta_with_riichi" :label="game.players[Winds.WEST].name"/>
-    <el-table-column prop="north_points_delta_with_riichi" :label="game.players[Winds.NORTH].name"/>
+  <el-table
+    :data="game.log"
+    :default-sort="{ prop: 'log_index', order: 'descending' }"
+    style="width: 100%"
+  >
+    <el-table-column prop="log_index" label="手" />
+    <el-table-column prop="hand_signature" label="场" />
+    <el-table-column prop="beginning_riichi_sticks" label="开局供托" />
+    <el-table-column prop="result" label="结果" />
+    <el-table-column prop="east_points_delta_with_riichi" :label="game.players[Winds.EAST].name" />
+    <el-table-column
+      prop="south_points_delta_with_riichi"
+      :label="game.players[Winds.SOUTH].name"
+    />
+    <el-table-column prop="west_points_delta_with_riichi" :label="game.players[Winds.WEST].name" />
+    <el-table-column
+      prop="north_points_delta_with_riichi"
+      :label="game.players[Winds.NORTH].name"
+    />
   </el-table>
-  <div>
-    {{ game.log }}
-  </div>
 
   <!-- after-game view -->
   <div v-if="!game.on_going && game.finished">
@@ -133,11 +158,24 @@ const Winds = Object.freeze({
   NORTH: 'north'
 })
 
+const HandResults = Object.freeze({
+  TSUMO: 'tsumo',
+  RON: 'ron',
+  DRAW: 'draw'
+})
+
 const NextWindMap = Object.freeze({
   [Winds.EAST]: Winds.SOUTH,
   [Winds.SOUTH]: Winds.WEST,
   [Winds.WEST]: Winds.NORTH,
-  [Winds.NORTH]: Winds.EAST,
+  [Winds.NORTH]: Winds.EAST
+})
+
+const LastWindMap = Object.freeze({
+  [Winds.EAST]: Winds.NORTH,
+  [Winds.SOUTH]: Winds.EAST,
+  [Winds.WEST]: Winds.SOUTH,
+  [Winds.NORTH]: Winds.WEST
 })
 
 const WindsDisplayTextMap = Object.freeze({
@@ -145,8 +183,14 @@ const WindsDisplayTextMap = Object.freeze({
     [Winds.EAST]: '东',
     [Winds.SOUTH]: '南',
     [Winds.WEST]: '西',
-    [Winds.NORTH]: '北',
+    [Winds.NORTH]: '北'
   }
+})
+
+const ResultDisplayTextMap = Object.freeze({
+  [HandResults.DRAW]: '流局',
+  [HandResults.TSUMO]: '自摸',
+  [HandResults.RON]: '荣和'
 })
 
 const PointsLadder = Object.freeze({
@@ -157,8 +201,22 @@ const PointsLadder = Object.freeze({
   YAKUMAN: 'YAKUMAN'
 })
 
-const AllowedHans = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-const AllowedFus = [20, 25, 30, 40, 50, 60, 70, 80, 90, 100, 110]
+const PointsLadderDisplayMap = Object.freeze({
+  [PointsLadder.MANGAN]: '满贯',
+  [PointsLadder.HANEMAN]: '跳满',
+  [PointsLadder.BAIMAN]: '倍满',
+  [PointsLadder.SANBAIMAN]: '三倍满',
+  [PointsLadder.YAKUMAN]: '役满'
+})
+
+const AllowedHans = [1, 2, 3, 4].concat(Object.keys(PointsLadder))
+
+const AllowedFus = Object.freeze({
+  [1]: [20, 25, 30, 40, 50, 60, 70, 80, 90, 100, 110],
+  [2]: [20, 25, 30, 40, 50, 60, 70, 80, 90, 100, 110],
+  [3]: [20, 25, 30, 40, 50, 60, 70],
+  [4]: [20, 25, 30]
+})
 
 const RonPointsNonDealer = Object.freeze({
   [[1, 30]]: 1000,
@@ -343,6 +401,7 @@ function SetUpGame(game, rules) {
   game.value.log = []
   game.value.hand_results = {
     riichi: [],
+    tenpai: []
   }
   for (let [_, player] of Object.entries(game.value.players)) {
     player.starting_points = rules.starting_points
@@ -360,18 +419,19 @@ function SetUpNewHand(game, rules) {
     return
   }
 
-  let hand_results = game.value.hand_results;
-  const dealer = FindDealerId(game.value.players);
-  const all_last = IsAllLast(game, rules);
-  let renchan = false;
-  let honba_increase = false;
-  if (hand_results.result == "draw") {
-    honba_increase = true;
+  let hand_results = game.value.hand_results
+  const dealer = FindDealerId(game.value.players)
+  const all_last = IsAllLast(game, rules)
+  let renchan = false
+  let honba_increase = false
+  if (hand_results.result == 'draw') {
+    honba_increase = true
     if (game.value.hand_results.tenpai.includes(dealer)) {
-      renchan = (!all_last && rules.dealer_tenpai_renchan) || (all_last && rules.all_last_dealer_tenpai_renchan)
+      renchan =
+        (!all_last && rules.dealer_tenpai_renchan) ||
+        (all_last && rules.all_last_dealer_tenpai_renchan)
     }
-  }
-  else {
+  } else {
     if (game.value.hand_results.winner == dealer) {
       honba_increase = true
       renchan = !all_last || rules.all_last_dealer_win_renchan
@@ -379,15 +439,15 @@ function SetUpNewHand(game, rules) {
   }
 
   // Shift winds if game continues
-  const game_finished = all_last && !renchan;
+  const game_finished = all_last && !renchan
   if (!game_finished && !renchan) {
     for (let [_, player] of Object.entries(game.value.players)) {
-      player.current_wind = NextWindMap[player.current_wind]
+      player.current_wind = LastWindMap[player.current_wind]
     }
 
     const num_players = Object.keys(game.value.players).length
     if (game.value.hand == num_players) {
-      game.value.round_wind = NextWindMap[game.value.round_wind]
+      game.value.round_wind = LastWindMap[game.value.round_wind]
       game.value.hand = 1
     } else {
       game.value.hand += 1
@@ -396,15 +456,16 @@ function SetUpNewHand(game, rules) {
 
   // adjust honba and riichi sticks
   if (honba_increase) {
-    game.value.honba += 1;
+    game.value.honba += 1
   } else {
-    game.value.honba = 0;
+    game.value.honba = 0
   }
-  if (hand_results.result != "draw") {
+  if (hand_results.result != 'draw') {
     game.value.riichi_sticks = 0
   }
   game.value.hand_results = {
     riichi: [],
+    tenpai: []
   }
 
   Log(`Game info after setting up new hand: ${JSON.stringify(game.value)}`)
@@ -414,16 +475,16 @@ function SetUpNewHand(game, rules) {
 function FindDealerId(players) {
   for (const [player_id, info] of Object.entries(players)) {
     if (info.current_wind == Winds.EAST) {
-      return player_id;
+      return player_id
     }
   }
-  alert(`FindDealerId: Failed to find dealer in ${JSON.stringify(players)}`);
+  alert(`FindDealerId: Failed to find dealer in ${JSON.stringify(players)}`)
 }
 
 // Checks if current hand is all last
 function IsAllLast(game, rules) {
   const num_players = Object.keys(game.value.players).length
-  return (game.hand == num_players && rule.last_round_wind == game.round_wind)
+  return game.hand == num_players && rule.last_round_wind == game.round_wind
 }
 
 // Finishes the whole game.
@@ -445,7 +506,7 @@ function ResolvePointsDeltaOnDraw(game, rules) {
   Log(`ResolvePointsDeltaOnDraw`)
   const tenpai = game.value.hand_results.tenpai
   const num_players = Object.keys(game.value.players).length
-  const num_tenpai = tenpai.length
+  const num_tenpai = tenpai ? tenpai.length : 0
   const num_noten = num_players - num_tenpai
   if (tenpai === undefined || num_tenpai === 0 || num_tenpai === num_players) {
     return {}
@@ -476,12 +537,12 @@ function ResolvePointsDeltaOnTsumo(game, rules) {
   const han = game.value.hand_results.han
   const fu = game.value.hand_results.fu
   const dealer_win = IsDealer(game.value.players[winner])
-  const points_map = dealer_win? TsumoPointsDealer : TsumoPointsNonDealer
+  const key = GetPointMapKey(han, fu, rules)
 
   let points_delta = {}
   if (dealer_win) {
     const delta =
-      TsumoPointsDealer[[han, fu]] + (game.value.honba * rules.honba_points) / (num_players - 1)
+      TsumoPointsDealer[key] + (game.value.honba * rules.honba_points) / (num_players - 1)
     for (const [_, player_id] of Object.entries(Winds)) {
       if (player_id == winner) {
         points_delta[player_id] = 3 * delta + game.value.riichi_sticks * rules.riichi_cost
@@ -490,7 +551,7 @@ function ResolvePointsDeltaOnTsumo(game, rules) {
       }
     }
   } else {
-    let [raw_non_dealer_delta, raw_dealer_delta] = TsumoPointsNonDealer[[han, fu]]
+    let [raw_non_dealer_delta, raw_dealer_delta] = TsumoPointsNonDealer[key]
     const non_dealer_delta =
       -raw_non_dealer_delta - (game.value.honba * rules.honba_points) / (num_players - 1)
     const dealer_delta =
@@ -513,6 +574,41 @@ function ResolvePointsDeltaOnTsumo(game, rules) {
   return points_delta
 }
 
+function GetPointMapKey(han, fu, rules) {
+  if (PointsLadder.hasOwnProperty(han)) {
+    return han
+  }
+  if (han < 3) {
+    return [han, fu]
+  } else if (han == 3) {
+    if (fu >= 70) {
+      return PointsLadder.MANGAN
+    } else if (fu == 60 && rules.round_up_mangan) {
+      return PointsLadder.MANGAN
+    } else {
+      return [han, fu]
+    }
+  } else if (han == 4) {
+    if (fu >= 40) {
+      return PointsLadder.MANGAN
+    } else if (fu == 30 && rules.round_up_mangan) {
+      return PointsLadder.MANGAN
+    } else {
+      return [han, fu]
+    }
+  } else if (han == 5) {
+    return PointsLadder.MANGAN
+  } else if (han < 8) {
+    return PointsLadder.HANEMAN
+  } else if (han < 11) {
+    return PointsLadder.BAIMAN
+  } else if (han == 13) {
+    return PointsLadder.SANBAIMAN
+  } else {
+    return PointsLadder.YAKUMAN
+  }
+}
+
 // Helper function, resolves points changes in the case of ron.
 function ResolvePointsDeltaOnRon(game, rules) {
   Log(`ResolvePointsDeltaOnRon`)
@@ -521,7 +617,8 @@ function ResolvePointsDeltaOnRon(game, rules) {
   const han = game.value.hand_results.han
   const fu = game.value.hand_results.fu
   const points_map = IsDealer(game.value.players[winner]) ? RonPointsDealer : RonPointsNonDealer
-  const delta = points_map[[han, fu]] + game.value.honba * rules.honba_points
+  const key = GetPointMapKey(han, fu, rules)
+  const delta = points_map[key] + game.value.honba * rules.honba_points
 
   let points_delta = {}
   points_delta[winner] = delta + game.value.riichi_sticks * rules.riichi_cost
@@ -565,20 +662,45 @@ function FinishCurrentHand(game, rules) {
     hand: game.value.hand,
     honba: game.value.honba,
     hand_signature: `${WindsDisplayTextMap.wind_character[game.value.round_wind]}${game.value.hand}-${game.value.honba}`,
-    riichi: game.value.hand_results.riichi,
+    beginning_riichi_sticks: game.value.riichi_sticks - game.value.hand_results.riichi.length,
+    result: ResultDisplayTextMap[game.value.hand_results.result],
+    riichi: game.value.hand_results.riichi
   }
   for (const [_, player_id] of Object.entries(Winds)) {
-    const delta = (points_delta.hasOwnProperty(player_id) ? points_delta[player_id] : 0)
-    const riichi = game.value.hand_results.riichi.includes(player_id);
-    hand_log[`${player_id}_points_delta_with_riichi`] = `${delta - (riichi ? rules.riichi_cost : 0)}`;
+    const delta = points_delta.hasOwnProperty(player_id) ? points_delta[player_id] : 0
+    const riichi = game.value.hand_results.riichi.includes(player_id)
+    hand_log[`${player_id}_points_delta_with_riichi`] =
+      `${delta - (riichi ? rules.riichi_cost : 0)}`
     if (riichi) {
-      hand_log[`${player_id}_points_delta_with_riichi`] += "(立直)"
+      hand_log[`${player_id}_points_delta_with_riichi`] += '(立直)'
+    }
+  }
+
+  if (game.value.hand_results.result == HandResults.DRAW) {
+    hand_log.result += `[${game.value.hand_results.tenpai.map((x) => {
+      return game.value.players[x].name
+    })}]`
+  } else if (game.value.hand_results.result == HandResults.TSUMO) {
+    hand_log.result = `${game.value.players[game.value.hand_results.winner].name}` + hand_log.result
+    const key = GetPointMapKey(game.value.hand_results.han, game.value.hand_results.fu)
+    if (PointsLadderDisplayMap.hasOwnProperty(key)) {
+      hand_log.result += `[${PointsLadderDisplayMap[key]}]`
+    } else {
+      hand_log.result += `[${key}]`
+    }
+  } else if (game.value.hand_results.result == HandResults.RON) {
+    hand_log.result = `${game.value.players[game.value.hand_results.winner].name}` + hand_log.result
+    const key = GetPointMapKey(game.value.hand_results.han, game.value.hand_results.fu)
+    if (PointsLadderDisplayMap.hasOwnProperty(key)) {
+      hand_log.result += `[${PointsLadderDisplayMap[key]}]`
+    } else {
+      hand_log.result += `[${key}]`
     }
   }
   game.value.log.push(hand_log)
 
   // set up a next hand.
-  SetUpNewHand(game, rules);
+  SetUpNewHand(game, rules)
 }
 
 // Handle player riichi event
@@ -588,8 +710,7 @@ function HandlePlayerRiichi(player_id, game, rules) {
   if (game.value.hand_results.riichi.includes(player_id)) {
     player.points -= rules.riichi_cost
     game.value.riichi_sticks += 1
-  }
-  else {
+  } else {
     player.points += rules.riichi_cost
     game.value.riichi_sticks -= 1
   }
@@ -605,7 +726,7 @@ const rules = ref({
   last_round_wind: Winds.SOUTH,
   dealer_tenpai_renchan: true,
   all_last_dealer_win_renchan: true,
-  all_last_dealer_tenpai_renchan: true,
+  all_last_dealer_tenpai_renchan: true
 })
 
 const game = ref({
@@ -651,6 +772,10 @@ export default {
       game: game,
       Winds: Winds,
       WindsDisplayTextMap: WindsDisplayTextMap,
+      HandResults: HandResults,
+      ResultDisplayTextMap: ResultDisplayTextMap,
+      PointsLadder: PointsLadder,
+      PointsLadderDisplayMap: PointsLadderDisplayMap,
       AllowedHans: AllowedHans,
       AllowedFus: AllowedFus
     }
