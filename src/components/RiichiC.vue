377fe1780bc25ac6ff06c9aaf15c578f73abf533
gaohan <gaohanm@icloud.com>
2024-02-16 15:47:26 -0500

    Revert "enforce font color"
    This reverts commit ff9b7782fdd6860724089fb49d80fa7c1b20289e.

--- src/components/RiichiC.vue
+++ src/components/RiichiC.vue
@@ -554,14 +554,11 @@ export default {
   top: 0;
   left: 0;
   background-color: white;
-  color: black;
 }
 
 .player_name_input {
   position: relative;
   font-size: 16px;
-  background-color: white;
-  color: black;
 }
 
 .gameboard {
@@ -573,7 +570,6 @@ export default {
   margin-top: 30px;
   margin-bottom: 30px;
   background-color: white;
-  color: black;
 }
 
 .hand_info_board {
@@ -585,8 +581,6 @@ export default {
   top: 210px;
   left: 110px;
   font-size: 16px;
-  background-color: white;
-  color: black;
 }
 
 .east_player_board,
