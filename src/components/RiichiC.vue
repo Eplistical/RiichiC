9fd49fc8c7b33972688932cc13e8ab67db88ef4d
gaohan <gaohanm@icloud.com>
2024-02-17 15:53:16 -0500

    Rewrite with composition API

--- src/components/RiichiC.vue
+++ src/components/RiichiC.vue
@@ -1,13 +1,99 @@
-<script setup></script>
+<script setup>
+import { ref, computed, onMounted, onDeactivated } from 'vue'
+import { MLeagueRuleset } from './rulesets.ts'
+import { Game } from './game.ts'
+import RuleSetConfigurationBoard from './RuleSetConfigurationBoard.vue'
+import GameBoard from './GameBoard.vue'
+import HandResultsInputBoard from './HandResultsInputBoard.vue'
+
+function preventRefresh(event) {
+  event.preventDefault()
+  event.returnValue = ''
+}
+
+onMounted(() => {
+  window.addEventListener('beforeunload', preventRefresh)
+})
+
+onDeactivated(() => {
+  window.removeEventListener('beforeunload', preventRefresh)
+})
+
+const player_names = ref(['赤木', '原田', '瓦西子', '天'])
+const ruleset = ref({ ...MLeagueRuleset })
+const game = ref(new Game())
+const hand_results_form = ref({})
+
+const ConfirmResetGameLogText = computed(() => {
+  return `确定重置？`
+})
+const ConfirmFinishGameText = computed(() => {
+  return `确定结束？`
+})
+const ConfirmSetUpNewGameText = computed(() => {
+  return `确定开始新游戏？数据不会保存!`
+})
+
+function StartGame() {
+  console.log('Start game with players: ', player_names.value, ' and ruleset: ', ruleset.value)
+  game.value.InitGame({ ruleset: ruleset.value, player_names: player_names.value })
+  game.value.Start()
+  game.value.StartCurrentHand()
+}
+
+function SetUpNewGame() {
+  if (!confirm(ConfirmSetUpNewGameText.value)) {
+    return
+  }
+  game.value = new Game()
+}
+
+function FinishGame() {
+  console.log('FinishGame')
+  if (!confirm(ConfirmFinishGameText.value)) {
+    return
+  }
+  if (game.value.Finish()) {
+    hand_results_form.value = {}
+  }
+}
+
+function SubmitHandResultsForm() {
+  console.log('SubmitHandResultsForm')
+  const hand_finished = game.value.FinishCurrentHand(hand_results_form.value)
+  if (hand_finished) {
+    game.value.SaveHandLog()
+    game.value.SetUpNextHandOrFinishGame()
+    game.value.StartCurrentHand()
+    hand_results_form.value = {}
+  }
+}
+
+function HandleResetGameLog(index, row) {
+  console.log('HandleResetGameLog: ', index, ' ', row)
+  if (!confirm(ConfirmResetGameLogText.value)) {
+    return
+  }
+  if (row.log_index >= game.value.log.length) {
+    alert(`cannot reset: log index ${row.log_index} >= log length ${game.value.log.length}`)
+  }
+  const hand_changed = game.value.ResetToPreviousFinishedHand(row.log_index)
+  if (hand_changed && game.value.current_hand.has_next_hand) {
+    // hands in log must be finished, thus we can move to next hand.
+    game.value.SetUpNextHandOrFinishGame()
+    game.value.StartCurrentHand()
+    hand_results_form.value = {}
+  }
+}
+</script>
 
 <template>
   <div class="app_board">
     <!-- Unstartd Game -->
-    <div v-if="GameIsNotStarted">
+    <div v-if="game.IsNotStarted()">
       <div class="player_name_configuration_board">
         <PlayerNameConfigurationBoard v-model="player_names" :num_players="ruleset.num_players" />
       </div>
-
       <div class="ruleset_configuration_board">
         <RuleSetConfigurationBoard v-model="ruleset" />
       </div>
@@ -21,7 +107,7 @@
 
       <el-divider />
 
-      <div class="hand_results_input_board" v-if="GameIsOnGoing">
+      <div class="hand_results_input_board" v-if="game.IsOnGoing()">
         <HandResultsInputBoard
           v-model="hand_results_form"
           :game="game"
@@ -32,8 +118,7 @@
       <div class="game_log_board">
         <GameLogBoard :game="game" @resetLog="HandleResetGameLog" />
       </div>
-
-      <div class="game_stats_board" v-if="GameIsFinished">
+      <div class="game_stats_board" v-if="game.IsFinished()">
         <GameStatsBoard :game="game" />
       </div>
     </div>
@@ -52,298 +137,6 @@
   </div>
 </template>
 
-<script>
-import { ElButton } from 'element-plus'
-import { Winds, WindsInOrder, WindsDisplayTextMap } from './seat_constants.ts'
-import { HandOutcomeEnum, HandOutcomeEnumDisplayTextMap } from './hand.ts'
-import {
-  PointsLadder,
-  PointsLadderDisplayMap,
-  PointsLadderBriefDisplayMap,
-  AllowedHans,
-  AllowedFus,
-  NumberDisplayMap
-} from './game_constants.ts'
-import { MLeagueRuleset } from './rulesets.ts'
-import { PlayerIdsInOrder } from './players.ts'
-import { Game, GameState } from './game.ts'
-import RuleSetConfigurationBoard from './RuleSetConfigurationBoard.vue'
-import GameBoard from './GameBoard.vue'
-import HandResultsInputBoard from './HandResultsInputBoard.vue'
-
-export default {
-  name: 'RiichiC',
-  components: {
-    ElButton
-  },
-  setup() {},
-  mounted() {},
-  beforeMount() {
-    window.addEventListener('beforeunload', this.preventRefresh)
-  },
-  beforeDestroy() {
-    window.removeEventListener('beforeunload', this.preventRefresh)
-  },
-  data() {
-    return {
-      player_names: ['赤木', '原田', '瓦西子', '天'],
-      ruleset: { ...MLeagueRuleset },
-      game: new Game(),
-      hand_results_form: {},
-
-      Winds: Winds,
-      WindsInOrder: WindsInOrder,
-      WindsDisplayTextMap: WindsDisplayTextMap,
-      PlayerIdsInOrder: PlayerIdsInOrder,
-      HandOutcomeEnum: HandOutcomeEnum,
-      HandOutcomeEnumDisplayTextMap: HandOutcomeEnumDisplayTextMap,
-      PointsLadder: PointsLadder,
-      PointsLadderDisplayMap: PointsLadderDisplayMap,
-      PointsLadderBriefDisplayMap: PointsLadderBriefDisplayMap,
-      AllowedHans: AllowedHans,
-      AllowedFus: AllowedFus
-    }
-  },
-  computed: {
-    GameIsNotStarted() {
-      return this.game.state == GameState.NOT_STARTED
-    },
-    GameIsOnGoing() {
-      return this.game.state == GameState.ON_GOING
-    },
-    GameIsFinished() {
-      return this.game.state == GameState.FINISHED
-    },
-    CurrentHandText() {
-      return `${WindsDisplayTextMap[this.game.current_hand.round_wind]}${this.game.current_hand.hand}局${this.game.current_hand.honba}本场`
-    },
-    RiichiSticksText() {
-      return `供托: ${this.game.current_hand.riichi_sticks}`
-    },
-    GameLogBoard() {
-      let board = []
-      for (let i = 0; i < this.game.log.length; ++i) {
-        const log = this.game.log[i]
-        const hand = log.hand
-        const players = log.players
-        const last_log = i > 0 ? this.game.log[i - 1] : null
-        const last_hand = last_log ? last_log.hand : null
-        const last_hand_players = last_log ? last_log.players : null
-        let row = {}
-        row.log_index = i
-        row.hand_signature = `${WindsDisplayTextMap[hand.round_wind]}${hand.hand}-${hand.honba}`
-        row.end_game_riichi_sticks = hand.riichi_sticks
-        row.start_game_riichi_sticks = last_hand ? last_hand.riichi_sticks : 0
-        row.results_summary = `${HandOutcomeEnumDisplayTextMap[hand.results.outcome]}`
-
-        if (
-          hand.results.outcome == HandOutcomeEnum.RON ||
-          hand.results.outcome == HandOutcomeEnum.TSUMO
-        ) {
-          if (typeof hand.results.han == 'string') {
-            row.results_summary += `[${PointsLadderBriefDisplayMap[hand.results.han]}]`
-          } else {
-            row.results_summary += `[${hand.results.han},${hand.results.fu}]`
-          }
-        }
-        for (const player_id of PlayerIdsInOrder) {
-          const end_hand_pt = players.GetPlayer(player_id).points
-          const start_hand_pt = last_hand_players
-            ? last_hand_players.GetPlayer(player_id).points
-            : this.game.ruleset.starting_points
-          const pt_delta = end_hand_pt - start_hand_pt
-          row[player_id] = `${end_hand_pt}`
-          if (pt_delta > 0) {
-            row[player_id] += `(+${pt_delta})`
-          } else if (pt_delta < 0) {
-            row[player_id] += `(${pt_delta})`
-          }
-          if (hand.riichi.has(player_id)) {
-            row[player_id] += `[立]`
-          }
-          if (
-            hand.results.outcome == HandOutcomeEnum.DRAW &&
-            hand.results.tenpai.includes(player_id)
-          ) {
-            row[player_id] += `[听]`
-          }
-          if (
-            (hand.results.outcome == HandOutcomeEnum.TSUMO ||
-              hand.results.outcome == HandOutcomeEnum.RON) &&
-            hand.results.winner == player_id
-          ) {
-            row[player_id] += `[和]`
-          }
-          if (hand.results.outcome == HandOutcomeEnum.RON && hand.results.deal_in == player_id) {
-            row[player_id] += `[铳]`
-          }
-        }
-        board.push(row)
-      }
-      return board.reverse()
-    },
-    GameStatsBoard() {
-      console.log('Generate GameStatsBoard')
-      let board = []
-      // scan log to compute stats
-      for (const player_id of PlayerIdsInOrder) {
-        let row = {}
-        row.points = this.GetPlayerPoints(player_id)
-        // find players rank from current hand
-        if (this.game.players) {
-          const current_points = this.game.players
-            ? this.game.players.GetPlayers(PlayerIdsInOrder).map((p) => p.points)
-            : [0, 0, 0, 0]
-          const pt = this.game.players.GetPlayer(player_id).points
-          row.rank = current_points.reduce((acc, val) => {
-            return val > pt ? acc + 1 : acc
-          }, 1)
-        } else {
-          row.rank = -1
-        }
-        row.player = `${this.GetPlayerName(player_id)}[${WindsDisplayTextMap[player_id]}起][${NumberDisplayMap[row.rank]}位]`
-        // loop log to compute game stats
-        let stats = {
-          riichi: 0,
-          agari: 0,
-          tsumo: 0,
-          ron: 0,
-          deal_in: 0,
-          riichi_agari: 0,
-          riichi_tsumo: 0,
-          riichi_ron: 0,
-          riichi_deal_in: 0,
-          tenpai_on_draw: 0,
-          agari_over_mangan: 0,
-          deal_in_over_mangan: 0
-        }
-        this.game.log.forEach((log) => {
-          const hand = log.hand
-          const player_riichi = log.hand.riichi && log.hand.riichi.has(player_id)
-          if (player_riichi) {
-            stats.riichi += 1
-          }
-          if (
-            hand.results.outcome == HandOutcomeEnum.DRAW &&
-            hand.results.tenpai &&
-            hand.results.tenpai.includes(player_id)
-          ) {
-            stats.tenpai_on_draw += 1
-          }
-          if (hand.results.outcome != HandOutcomeEnum.DRAW && hand.results.winner == player_id) {
-            stats.agari += 1
-            if (player_riichi) {
-              stats.riichi_agari += 1
-            }
-            if (hand.results.outcome != HandOutcomeEnum.RON) {
-              stats.riichi_ron += 1
-            }
-            if (hand.results.outcome != HandOutcomeEnum.TSUMO) {
-              stats.riichi_tsumo += 1
-            }
-            if (hand.results.han in PointsLadder) {
-              stats.agari_over_mangan += 1
-            }
-          }
-          if (hand.results.outcome == HandOutcomeEnum.RON && hand.results.deal_in == player_id) {
-            stats.deal_in += 1
-            if (player_riichi) {
-              stats.riichi_deal_in += 1
-            }
-            if (hand.results.han in PointsLadder) {
-              stats.deal_in_over_mangan += 1
-            }
-          }
-        })
-        stats.riichi_agari_rate = stats.riichi ? stats.riichi_agari / stats.riichi : 0.0
-        stats.riichi_tsumo_rate = stats.riichi ? stats.riichi_tsumo / stats.riichi : 0.0
-        stats.riichi_deal_in_rate = stats.riichi ? stats.riichi_deal_in / stats.riichi : 0.0
-        stats.agari_with_over_mangan_agari = `${stats.agari}(${stats.agari_over_mangan})`
-        stats.deal_in_with_over_mangan_deal_in = `${stats.deal_in}(${stats.deal_in_over_mangan})`
-        Object.assign(row, stats)
-        board.push(row)
-      }
-      return board
-    }
-  },
-  methods: {
-    StartGame() {
-      console.log(
-        `Start game with players ${JSON.stringify(this.player_names)} and ruleset ${JSON.stringify(this.ruleset)}`
-      )
-      this.game.InitGame({ ruleset: this.ruleset, player_names: this.player_names })
-      this.game.Start()
-      this.game.StartCurrentHand()
-    },
-    SetUpNewGame() {
-      if (!confirm('确定开始新游戏？数据不会保存!')) {
-        return
-      }
-      this.game = new Game()
-    },
-    FinishGame() {
-      console.log('FinishGame')
-      if (!confirm(`确定结束？`)) {
-        return
-      }
-      if (this.game.Finish()) {
-        this.hand_results_form = {}
-      }
-    },
-    HandlePlayerRiichi(player_id, riichi) {
-      console.log(`HandlePlayerRiichi: ${player_id}, ${riichi}`)
-      if (riichi == true) {
-        this.game.PlayerRiichi(player_id)
-      } else {
-        this.game.PlayerUnRiichi(player_id)
-      }
-    },
-    SubmitHandResultsForm() {
-      console.log('SubmitHandResultsForm')
-      const hand_finished = this.game.FinishCurrentHand(this.hand_results_form)
-      if (hand_finished) {
-        this.game.SaveHandLog()
-        this.game.SetUpNextHandOrFinishGame()
-        this.game.StartCurrentHand()
-        this.hand_results_form = {}
-      }
-    },
-    HandleResetGameLog(index, row) {
-      console.log('HandleResetGameLog: ', index, ' ', row)
-      if (!confirm(`确定重置？`)) {
-        return
-      }
-      if (row.log_index >= this.game.log.length) {
-        alert(`cannot reset: log index ${row.log_index} >= log length ${this.game.log.length}`)
-      }
-      const hand_changed = this.game.ResetToPreviousFinishedHand(row.log_index)
-      if (hand_changed && this.game.current_hand.has_next_hand) {
-        // hands in log must be finished, thus we can move to next hand.
-        this.game.SetUpNextHandOrFinishGame()
-        this.game.StartCurrentHand()
-        this.hand_results_form = {}
-      }
-    },
-    GetPlayerName(player_id) {
-      return this.game.players.GetPlayer(player_id).name
-    },
-    GetPlayerCurrentWind(player_id) {
-      return this.game.players.GetPlayer(player_id).current_wind
-    },
-    GetPlayerPoints(player_id) {
-      return this.game.players.GetPlayer(player_id).points
-    },
-    IsDealer(player_id) {
-      return this.game.players.GetPlayer(player_id).IsDealer()
-    },
-    preventRefresh(event) {
-      event.preventDefault()
-      event.returnValue = ''
-    }
-  }
-}
-</script>
-
 <style scoped>
 .app_board {
   position: absolute;
@@ -354,12 +147,6 @@ export default {
   background-color: white;
   color: black;
 }
-
-.player_name_input {
-  position: relative;
-  font-size: 16px;
-}
-
 .gameboard {
   position: relative;
   border-style: solid;
