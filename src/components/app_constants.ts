50cc304e7e462170ad1f74fcf6fc3311b347eb7b
gaohan <gaohanm@icloud.com>
2024-05-25 19:43:06 -0400

    sv

--- src/components/app_constants.ts
+++ src/components/app_constants.ts
@@ -3,18 +3,16 @@ export const AppMode = Object.freeze({
   LEADER_BOARD: 'LEADER_BOARD'
 })
 
-/*
 export const API_URL = 'https://uf7tin6si3sgnif7truyy3rrwm0kzqjd.lambda-url.us-east-2.on.aws'
 export const GET_STATS_API = API_URL
 export const LIST_GAMES_API = API_URL
 export const RECORD_GAME_API = API_URL
-*/
 
+/*
 export const GET_STATS_API = 'get_stats_api'
 export const LIST_GAMES_API = 'list_games_api'
 export const RECORD_GAME_API = 'record_game_api'
 
-/*
 export const GET_STATS_API = 'RiichiC/get_stats_api'
 export const LIST_GAMES_API = 'RiichiC/list_games_api'
 export const RECORD_GAME_API = 'RiichiC/record_game_api'
